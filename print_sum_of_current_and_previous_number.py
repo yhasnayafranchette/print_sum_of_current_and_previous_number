e78a7fa98c20138460a4bee41879a8d5919e4aba
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-22 12:21:36 +0800

    finalized

--- print_sum_of_current_and_previous_number.py
+++ print_sum_of_current_and_previous_number.py
@@ -1,9 +1,11 @@
 #Exercise 2: Print the sum of the current number and the previous number
 #Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
 
+#Set 0 as the previous number
 def sum_of_current_and_previous_number():
     previous_number = 0
 
+#Calculate sum of the current and previous number, then display result
     for current_number in range(0,10):
         sum_of_two_numbers = current_number + previous_number
         print(f"The sum of current number {current_number} and previous number {previous_number} is: {sum_of_two_numbers}")
